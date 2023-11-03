3ff888902c7e8e213ec14e3ad64f716b0662a195
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-11-03 11:49:02 -0300

    feat: config webpack file and install dependencies

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./src",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Battleship",
+      template: "src/index.html",
+      minify: {
+        collapseWhitespace: true,
+        keepClosingSlash: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+  ],
+};
