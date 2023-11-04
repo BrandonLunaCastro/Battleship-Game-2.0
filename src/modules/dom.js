725bfd032a6128d42fa8055778c6ac1666aac793
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-11-04 16:46:37 -0300

    feat: add function drawShips

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -3,14 +3,21 @@ function dom() {
     const fragment = document.createDocumentFragment();
     for (let i = 0; i < 100; i++) {
       const div = document.createElement("div");
-      div.classList.add(player);
-      div.setAttribute("coordinate", i);
+      div.classList.add("grid", player);
+      div.setAttribute("data-coordinate", i);
       fragment.appendChild(div);
     }
     reference.appendChild(fragment);
   };
 
-  return {createBoard};
+  const drawShips = ([...arrCoordinates], player) => {
+    for(const coord of arrCoordinates){
+      const referenceDom = document.querySelector(`.${player.name}[data-coordinate="${coord}"]`);
+      referenceDom.classList.add("ship");
+    }
+  };
+
+  return {createBoard, drawShips};
 }
 
 export { dom };
