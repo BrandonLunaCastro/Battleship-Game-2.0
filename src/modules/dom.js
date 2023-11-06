c75feef4d28b82773d2d720c1d9b05ffb4e2b1ec
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-11-06 10:47:20 -0300

    feat: add helpers function

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -17,7 +17,48 @@ function dom() {
     }
   };
 
-  return {createBoard, drawShips};
+  const verifyIsSunk = (board, player) => {
+    if (board.allSunk()) {
+      console.log(`the winner is ${player}`);
+    };
+  };
+
+  const attackAI = (e) => {
+
+  }
+
+  const stateOfAttack = () => {
+    
+  }
+
+  const attackShip = (reference, board, player) => {
+    document.querySelector(reference).addEventListener("click", e => {
+      if (!e.target.matches(".grid"))return false;
+      
+      console.log( e.target.dataset.coordinate);
+      const coordinate = parseInt(e.target.dataset.coordinate);
+      const stateAttack = board.receiveAttack(coordinate);
+
+      if (stateAttack === "already attacked") console.log("try again, this coordinate already been attacked");
+      if (stateAttack) {
+        e.target.classList.add("hit");
+        verifyIsSunk(board, player);
+      } else {
+        e.target.classList.add("fail");
+        verifyIsSunk(board, player);
+      };
+
+    });
+  };
+
+  const turnOf = (player) => {
+    const turnOfPlayer = player.id === 1 ? "human" : "machine";
+    document.querySelector(".turn").textContent =`Turn of : ${turnOfPlayer}`;
+    if (turnOfPlayer === "human")
+  };
+
+  return {createBoard, drawShips, attackShip, turnOf};
+
 }
 
 export { dom };
