7125510668ab60dac67cb8b008ea0a4fdb94d6c7
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-11-06 10:49:18 -0300

    feat: add id property to factory Player

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,27 +1,25 @@
-function Player(name) {
- 
-  const randomAttack = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
-    
+function Player(name, id) {
+  const randomAttack = (max, min) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+
   const attackSended = [];
-  
+
   const attackAI = () => {
     let attempt = true;
-    while(attempt){
+    while (attempt) {
       const attack = randomAttack(99, 0);
       console.log(attack);
-      if(!attackSended.includes(attack)) {
+      if (!attackSended.includes(attack)) {
         attackSended.push(attack);
         attempt = false;
         return attack;
       }
     }
-
   };
-  return { name, attackAI };
-}; 
-  
-  
+
+  return { name, id, attackAI };
+}
 
 // module.exports = Player;
 
-export {Player};
+export { Player };
