e9a380fa3d40a0ac0005f87c28260488288011e2
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-11-06 10:47:57 -0300

    feat: add some verifications to receiveAttack

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -1,10 +1,9 @@
-// const Ship = require("./ship");
-import {Ship} from "./ship.js";
+import { Ship } from "./ship.js";
 
 function gameBoard() {
   const board = [];
   const allShips = [];
-  const attackFailed = [];
+  const attackSended = [];
   const coordinateShips = [];
   // create gameBoard on the memory
   for (let i = 0; i < 100; i++) {
@@ -23,33 +22,45 @@ function gameBoard() {
       }
       return board;
     } else {
-      return "already ship here";
+      return "already place here";
     }
   };
 
   const verifyShip = (name) => allShips.filter((ship) => ship.name === name);
 
+  const verifyAttack = (coord) => attackSended.includes(parseInt(coord));
+  
   const receiveAttack = (attackCoordinates) => {
-    if (board[attackCoordinates] !== null) {
+    if (board[attackCoordinates] !== null && !verifyAttack(attackCoordinates)) {
       const nameShip = board[attackCoordinates];
       const shipAttacked = verifyShip(nameShip);
+      attackSended.push(attackCoordinates);
       shipAttacked[0].hit();
+      board[attackCoordinates] = "sunk";
       return true;
     }
 
     if (board[attackCoordinates] === null) {
       board[attackCoordinates] = "fail";
-      attackFailed.push(attackCoordinates);
+      attackSended.push(attackCoordinates);
       return false;
     }
+
+    return "already attacked";
   };
 
   const allSunk = () => {
     return allShips.every((ship) => ship.isSunk());
   };
 
-  return { board, coordinateShips, placeShip, verifySpace, receiveAttack, allSunk };
+  return {
+    board,
+    coordinateShips,
+    placeShip,
+    verifySpace,
+    receiveAttack,
+    allSunk,
+  };
 }
 
-
-export {gameBoard};
+export { gameBoard };
